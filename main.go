fbda54e2b784eaab3512c58962e16098ab268d48
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-02-03 15:00:28 +0530

    Added Sha hashing

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
     "net/http"
     "github.com/gorilla/mux"
+    "crypto/sha256"
+    "encoding/base64"
 )
 
 func main(){
@@ -18,11 +20,18 @@ func main(){
 
 func FileUploadHandler(w http.ResponseWriter, r *http.Request){
 
+    sha := sha256.New()
+
+
     r.ParseForm()
 
     data := r.Form["data"][0]
 
-    w.Write([]byte(data))
+    hashOfFile := sha.Sum([]byte(data))
+    hashString := base64.URLEncoding.EncodeToString(hashOfFile)
+
+    //w.Write([]byte(data))
+    w.Write([]byte(hashString))
 
 
 }
