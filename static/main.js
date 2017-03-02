b3e1c6320862272af24881c3f64f1ee4bf3b40eb
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-03-02 16:43:29 +0530

    Removed unused tags in the front end

--- static/main.js
+++ static/main.js
@@ -11,9 +11,7 @@ $(function() {
   $('#dropzone input').on('change', function(e) {
     var file = this.files[0];
 
-    $('#dropzone').removeClass('hover');
-
-    
+    $('#dropzone').removeClass('hover');    
     $('#dropzone').addClass('dropped');
     $('#dropzone img').remove();
     
@@ -21,13 +19,9 @@ $(function() {
 
       reader.readAsBinaryString(file);
 
-      console.log(file);
-      
       reader.onload = function(e) {
         var data = e.target.result;
 
-        console.log(data);
-        
         $.post("/file", {data: data}, function(data){
           document.write(data)
         })
