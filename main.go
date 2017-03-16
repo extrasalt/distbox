9b990ec9fe7cb5b504ab7b7f35ff80e37e42d6e3
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-03-16 14:53:15 +0530

    Refactored authentication login into separate file

--- main.go
+++ main.go
@@ -5,11 +5,9 @@ import (
 	"crypto/sha256"
 	"database/sql"
 	"encoding/hex"
-	"fmt"
 	"github.com/gorilla/mux"
 	ipfs "github.com/ipfs/go-ipfs-api"
 	_ "github.com/lib/pq"
-	"golang.org/x/crypto/bcrypt"
 	"io"
 	"net/http"
 )
@@ -111,94 +109,5 @@ func FileUploadHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("\n\n"))
 
 	w.Write([]byte((key)))
-
-}
-
-func authenticate(next http.HandlerFunc) http.HandlerFunc {
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		_, err := r.Cookie("rcs")
-		if err != nil {
-			http.Redirect(w, r, "/login", 302)
-		} else {
-			next(w, r)
-		}
-
-	}
-
-}
-
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		panic(err)
-	}
-
-	username := r.Form["name"][0]
-	password := r.Form["password"][0]
-
-	err = authorize(username, password)
-
-	if err == nil {
-		cookie := &http.Cookie{Name: "rcs", Value: username, MaxAge: 3600, Secure: false, HttpOnly: true, Raw: username}
-		http.SetCookie(w, cookie)
-		w.Write([]byte("authenticated"))
-	} else {
-		w.Write([]byte("Wrong password"))
-	}
-}
-
-func SignUpHandler(w http.ResponseWriter, r *http.Request) {
-
-	err := r.ParseForm()
-
-	if err != nil {
-		panic(err)
-	}
-
-	username := r.Form["name"][0]
-	password := r.Form["password"][0]
-
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	_, err = DB.Exec("insert into login values($1, $2)", username, hashedPassword)
-	http.Redirect(w, r, "/", 302)
-}
-
-func authorize(username string, password string) (autherr error) {
-
-	var dbpassword string
-
-	rows, err := DB.Query("Select password from login where name=$1", username)
-
-	if err != nil {
-		panic(err)
-	}
-
-	for rows.Next() {
-		err = rows.Scan(&dbpassword)
-
-		if err != nil {
-			panic(err)
-		}
-
-		break
-
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(dbpassword), []byte(password))
-
-	if err == nil {
-
-		return nil
-
-	} else {
-		autherr = fmt.Errorf("Cannot authorize %q", username)
-
-		return autherr
-	}
 
 }
